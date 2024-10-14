bbda3723bc4c8ac8b35f3d51c665563d184dd2ec
katzhukk <katzhukova230@gmail.com>
2024-10-14 21:29:40 +0300

    added CalendarComponent, RandomUtils, RegFormJenkinsTest, RegistrationPage, resources, TableResults and TestBase

---
+++ src/test/java/RegFormJenkinsTest.java
@@ -0,0 +1,58 @@
+import org.junit.jupiter.api.Test;
+import pages.RegistrationPage;
+import utils.RandomUtils;
+
+public class RegFormJenkinsTest extends TestBase {
+    private final RegistrationPage registrationPage = new RegistrationPage();
+
+    String firstName = RandomUtils.getRandomFirstName(),
+            lastName = RandomUtils.getRandomLastName(),
+            userEmail = RandomUtils.getRandomEmail(),
+            gender = RandomUtils.getRandomGender(),
+            userNumber = RandomUtils.getRandomNumber(),
+            yearOfBirth = RandomUtils.getRandomYearOfBirth(),
+            monthOfBirth = RandomUtils.getRandomMonthOfBirth(),
+            dayOfBirth = RandomUtils.getRandomDayOfBirth(monthOfBirth, yearOfBirth),
+            subjects = RandomUtils.getRandomSubject(),
+            hobbies = RandomUtils.getRandomHobbies(),
+            picture = RandomUtils.getRandomPicture(),
+            currentAddress = RandomUtils.getRandomCurrentAddress(),
+            state = RandomUtils.getRandomState(),
+            city = RandomUtils.getRandomCity(state);
+
+    @Test
+    void successfulRegistrationTest() {
+        registrationPage.openPage()
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setUserEmail(userEmail)
+                .setUserNumber(userNumber)
+                .setGenderWrapper(gender)
+                .setDateOfBirth(dayOfBirth, monthOfBirth, yearOfBirth)
+                .setSubjects(subjects)
+                .setHobbiesWrapper(hobbies)
+                .setPicture(picture)
+                .setCurrentAddress(currentAddress)
+                .setState(state)
+                .setCity(city)
+                .clickSubmit();
+
+        registrationPage.checkResult("Student Name", firstName + " " + lastName)
+                .checkResult("Student Email", userEmail)
+                .checkResult("Gender", gender)
+                .checkResult("Mobile", userNumber)
+                .checkResult("Date of Birth", dayOfBirth + " " + monthOfBirth + "," + yearOfBirth)
+                .checkResult("Subjects", subjects)
+                .checkResult("Hobbies", hobbies)
+                .checkResult("Picture", picture)
+                .checkResult("Address", currentAddress)
+                .checkResult("State and City", state + " " + city);
+    }
+
+    @Test
+    void checkNegativeResult() {
+        registrationPage.openPage().clickSubmit();
+
+        registrationPage.checkNegativeResult();
+    }
+}
---
+++ src/test/java/TestBase.java
@@ -0,0 +1,20 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+
+    @BeforeAll
+    static void setup() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        //Configuration.holdBrowserOpen = true;
+    }
+
+    @AfterEach
+    void closeWebDriver() {
+        WebDriverRunner.closeWebDriver();
+    }
+}
